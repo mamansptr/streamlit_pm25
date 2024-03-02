88a7ac77c7c93fc3a3b7e1605123ba290a9765ea
Maman Saputra <mamansaputra11@gmail.com>
2024-03-03 05:56:59 +0700

    Create git init.py

---
+++ git init.py
@@ -0,0 +1,7 @@
+cd C:\Users\maman\OneDrive\Documents\GitHub\streamlit_pm25
+
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin <https://github.com/mamansptr/streamlit_pm25.git>
+git push -u origin master
